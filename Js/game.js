d849c405b40ed0423f480cccfaf79f33ae191301
Anderson Andrade <andersonfuzz@outlook.com>
2022-06-25 19:02:20 -0300

    retifiiicccc

--- Js/game.js
+++ Js/game.js
@@ -1,6 +1,6 @@
 const grid = document.querySelector(".grid");
 
-const character = ["1", "2", "3", "4", "5", "6", "7", "8",'9'];
+const character = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 const createElement = (tag, className) => {
   const element = document.createElement(tag);
@@ -8,52 +8,51 @@ const createElement = (tag, className) => {
   return element;
 };
 
-let firstCard=''
-let secondCard=''
-const checkEndGame=()=>{
-  const disabledCards=document.querySelectorAll('.disabled_card')
-  if(disabledCards.length===18){
-    alert('Você venceu!#Em desenvolvimento')
+let firstCard = "";
+let secondCard = "";
+const checkEndGame = () => {
+  const disabledCards = document.querySelectorAll(".disabled_card");
+  if (disabledCards.length === 18) {
+    alert("Você venceu!#Em desenvolvimento");
   }
-}
+};
 
-const checkCards=()=>{
-  const firsCharacter=firstCard.getAttribute('data_character')
-  const secondCharacter=secondCard.getAttribute('data_character')
-  if(firsCharacter==secondCharacter){
-    firstCard.firstChild.classList.add('disabled_card')
-    secondCard.firstChild.classList.add('disabled_card')
+const checkCards = () => {
+  const firsCharacter = firstCard.getAttribute("data_character");
+  const secondCharacter = secondCard.getAttribute("data_character");
+  if (firsCharacter == secondCharacter) {
+    firstCard.firstChild.classList.add("disabled_card");
+    secondCard.firstChild.classList.add("disabled_card");
 
-      firstCard=''
-      secondCard=''
+    firstCard = "";
+    secondCard = "";
 
-      checkEndGame()
-  }else {
-    setTimeout(()=>{
-      firstCard.classList.remove('reveal_card')
-      secondCard.classList.remove('reveal_card')
-      firstCard=''
-      secondCard=''
-    },500)
+    checkEndGame();
+  } else {
+    setTimeout(() => {
+      firstCard.classList.remove("reveal_card");
+      secondCard.classList.remove("reveal_card");
+      firstCard = "";
+      secondCard = "";
+    }, 500);
   }
-}
+};
 
-const reveaalCard=({target})=>{
-    if(target.parentNode.className.includes('reveal_card')){
-        return
-    }
+const reveaalCard = ({ target }) => {
+  if (target.parentNode.className.includes("reveal_card")) {
+    return;
+  }
 
-    if(firstCard===''){
-          target.parentNode.classList.add('reveal_card')
-          firstCard=target.parentNode;
-    }else if(secondCard===''){
-        target.parentNode.classList.add('reveal_card')
-        secondCard=target.parentNode;
-        
-        checkCards()
-    }
-    
-}
+  if (firstCard === "") {
+    target.parentNode.classList.add("reveal_card");
+    firstCard = target.parentNode;
+  } else if (secondCard === "") {
+    target.parentNode.classList.add("reveal_card");
+    secondCard = target.parentNode;
+
+    checkCards();
+  }
+};
 
 const createCard = (character) => {
   const card = createElement("div", "card");
@@ -64,17 +63,15 @@ const createCard = (character) => {
 
   card.appendChild(front);
   card.appendChild(back);
-  card.addEventListener('click',reveaalCard)
-  card.setAttribute('data_character',character)
+  card.addEventListener("click", reveaalCard);
+  card.setAttribute("data_character", character);
   return card;
 };
 
 const loadGame = () => {
-
   const duplicateCharacters = [...character, ...character];
 
-  const shuffledArray = duplicateCharacters.sort(()=>Math.random()-0.5);
-
+  const shuffledArray = duplicateCharacters.sort(() => Math.random() - 0.5);
 
   shuffledArray.forEach((character) => {
     const card = createCard(character);
